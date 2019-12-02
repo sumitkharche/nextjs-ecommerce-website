fdb6b13a991ffb05471527935df40ec474b419a1
Sumit Kharche <43902034+sumitkharche@users.noreply.github.com>
2019-12-03 02:07:10 +0530

    updated snipcart api key

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ export default {
       read_key: process.env.COSMIC_READ_KEY || 'ICEOPViWhe1cc7qyOXapfUI2l7jwTO8FPIdQg8yagMjkCpi9T7',
       write_key: process.env.COSMIC_WRITE_KEY || '',
       port: process.env.PORT || '',
-      snipcart_api_key: process.env.SNIPCART_KEY || ''
+      snipcart_api_key: process.env.SNIPCART_KEY || 'OTMwZGE2NDQtZTY5NC00Zjc2LWI2M2QtNTEwNWI4NDNjNGNlNjM3MTA1NTk4NDk1OTE1NDQx'
     }
   }
